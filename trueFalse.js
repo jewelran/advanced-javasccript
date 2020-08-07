a3cc2b506b03bbf28fc207138f62965493e236c7
jewelran <mdjewelrana6487@gmail.com>
2020-08-07 23:09:56 +0600

    true vs false

---
+++ trueFalse.js
@@ -0,0 +1,21 @@
+// falsy..........
+// 0
+// ""
+// undefine
+// NaN
+// false;
+
+// truety
+
+// "0"
+//  " "
+//  true
+// []
+// {}
+
+const name = 0
+if (name  || name == 0) {
+    console.log("it is true")
+}else{
+    console.log("it is false")
+}
