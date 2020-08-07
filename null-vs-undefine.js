24e2ec1f409fe5af8283639322584f57223b78db
jewelran <mdjewelrana6487@gmail.com>
2020-08-07 23:43:09 +0600

    undefine vs  NaN

---
+++ null-vs-undefine.js
@@ -0,0 +1,19 @@
+let jewel;
+console.log(jewel)
+
+function num(num1,num2){
+    return 
+}
+const result  = num(34);
+console.log(result)
+
+const student = {name:"jewel" , id  :32,}
+console.log(student.phone)
+
+let fun = undefined;
+console.log(fun);
+
+
+let ages  =[12,434,232,34];
+console.log(ages[12])
+
