6921582288956c81b044f46ca7da5c3f625dd6be
jewelran <mdjewelrana6487@gmail.com>
2020-08-08 00:13:18 +0600

    triple-vs-double equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,20 @@
+// dounle  equal (==)
+const num1 = 2;
+const num2 = "2";
+if (num1 == num2 ) {
+    console.log("condition is true")
+}else{
+    console.log("condition is false")
+}
+// condition is true;
+
+// triple equal(===)
+const num3 = 2;
+const num4 = "2";
+if (num1 === num2 ) {
+    console.log("condition is true")
+}else{
+    console.log("condition is false")
+}
+
+// condition is false
