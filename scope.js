0914baf5f2096057b804cbfded17f8057fdba393
jewelran <mdjewelrana6487@gmail.com>
2020-08-08 22:05:02 +0600

    block scope, global scope

---
+++ scope.js
@@ -0,0 +1,16 @@
+function sum(first, secound){
+    let ruselt  = first + secound;
+    if (ruselt > 10) {
+    //    var  name = "jewel rana"
+   
+   const name = "jewel rana"
+        // name = "aboul kashem"
+        console.log(name)
+    }
+    // console.log(name)
+    return ruselt
+   
+}
+const output = sum(23, 54);
+console.log(output)
+
